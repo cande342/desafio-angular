2da26051d49412edc8c1478233a9afd3282a59cc
candela <gcandela894@gmail.com>
2024-10-10 03:32:31 -0300

    correccion paginacion

--- src/app/components/characters/characters.component.ts
+++ src/app/components/characters/characters.component.ts
@@ -9,72 +9,60 @@ import { CharactersService } from 'src/app/services/characters.service';
   styleUrls: ['./characters.component.css']
 })
 export class CharactersComponent implements OnInit, OnDestroy {
-  //Desde este componente manejamos toda la lógica del servicio, comunicando tanto al buscador como a 
-  //La lista de resultados.
+  //Este es el compontente principal, el que maneja toda la logica del servicio y pasa datos a Search y List.
 
   characters: Result[] | null = null;
   currentPage: number = 1;
   totalPages: number = 0;
-  errorMessage: string = ''; 
+  errorMessage: string = '';
+  searchTerm: string = ''; // Agregar esta propiedad para almacenar el término de búsqueda
 
-  //Las subscripciones las guardamos acá para desus todas juntas.
   private subscription: Subscription = new Subscription();
 
   constructor(private charactersService: CharactersService) {}
 
   ngOnInit(): void {
-    this.loadCharacters();
+    this.loadCharacters(); 
   }
 
-  loadCharacters(searchTerm: string = ''): void {
+  loadCharacters(searchTerm: string = this.searchTerm): void {
     const characterSubscription = this.charactersService.searchCharacters(searchTerm, this.currentPage)
       .subscribe(
         (data: RMCharacter) => {
           this.characters = data.results;
           this.totalPages = data.info.pages;
-          this.errorMessage = ''; 
+          this.errorMessage = '';
         },
         (error) => {
           console.log('Hubo un error al cargar personajes:', error.message);
           this.errorMessage = error.message;
         }
       );
-  
+
     this.subscription.add(characterSubscription);
   }
 
   onSearch(searchTerm: string): void {
-    const searchSubscription = this.charactersService.searchCharacters(searchTerm, this.currentPage)
-      .subscribe(
-        (data: RMCharacter) => {
-          this.characters = data.results;
-          this.totalPages = data.info.pages; 
-          this.errorMessage = ''; 
-        },
-        (error) => {
-          console.log('Hubo un error en la búsqueda:', error.message);
-          this.errorMessage = error.message;
-        }
-      );
-  
-    this.subscription.add(searchSubscription);
+    this.searchTerm = searchTerm; 
+    this.currentPage = 1; 
+    this.loadCharacters(this.searchTerm); 
   }
 
   nextPage(): void {
     if (this.currentPage < this.totalPages) {
       this.currentPage++;
-      this.loadCharacters();
+      this.loadCharacters(this.searchTerm);
     }
   }
 
   previousPage(): void {
     if (this.currentPage > 1) {
       this.currentPage--;
-      this.loadCharacters();
+      this.loadCharacters(this.searchTerm); 
     }
   }
 
-  ngOnDestroy() {
+  ngOnDestroy(): void {
     this.subscription.unsubscribe();
   }
 }
